0eb4b113c72d2359a115ae8671878dab223aa4d4
DrAlexanderDietz <alexander.dietz@excelitas.com>
2025-08-30 16:49:26 +0200

    Update my_nyquist_calculator.py
    add some text and color theme

--- my_nyquist_calculator.py
+++ my_nyquist_calculator.py
@@ -20,7 +20,7 @@ def opt_resolution(wavelength_nm, objective_mag, numerical_aperture, additional_
     return camera_res_um
 
 
-def plot_pxl_per_blurr(magnification, num_aprt, wavelength_nm, pxl_ref, additional_mag, xscale="log"):
+def plot_pxl_per_blurr(magnification, num_aprt, wavelength_nm, pxl_ref, additional_mag, xscale="log", yscale='lin'):
     """
     Generate heatmap of pixels-per-blur ratio.
     """
@@ -30,7 +30,7 @@ def plot_pxl_per_blurr(magnification, num_aprt, wavelength_nm, pxl_ref, addition
 
     fig, ax = plt.subplots(figsize=(7, 7))
 
-    #plt.style.use('dark_background')
+    #plt.style.use('ggplot')
 
     im = ax.imshow(
         Z,
@@ -43,13 +43,14 @@ def plot_pxl_per_blurr(magnification, num_aprt, wavelength_nm, pxl_ref, addition
         interpolation="none"
     )
 
-    ax.set_xlabel("Magnification")
-    ax.set_ylabel("Numerical Aperture")
-    ax.set_title(f"Optical Sampling Performance:\n "
-                 f"{pxl_ref:.1f} µm pixels, {wavelength_nm:.0f} nm, add. mag {additional_mag:.1f}×\n")
+    ax.set_xlabel("Magnification x")
+    ax.set_ylabel("Numerical Aperture NA")
+    ax.set_title("Optical Sampling Performance:\n \n"
+                 f"Settings: {pxl_ref:.1f} µm pixels | {wavelength_nm:.0f} nm | Add. Magn. {additional_mag:.1f}×\n")
     ax.grid(True, linestyle="dotted")
     ax.minorticks_on()
     ax.set_xscale(xscale)
+    ax.set_yscale(yscale)
 
     # Colorbar
     cbar = fig.colorbar(im, ax=ax, orientation="horizontal", pad=0.15)
@@ -68,19 +69,31 @@ def plot_pxl_per_blurr(magnification, num_aprt, wavelength_nm, pxl_ref, addition
 # ------------------- STREAMLIT APP -------------------
 st.title("pco.calculator - Nyquist Sampling")
 
+st.write("This is a small application to figure out which pixelsize best suits your microscope system. " \
+"Use the slider bars on the left side to set the illumination wavelength and the pixel pitch of your camera of interest, e.g. 2.5 µm " \
+"for pco.panda 26, 4.6 µm for pco.edge 9.4 bi or 6.5 µm for pco.edge 4.2."\
+ " Further, you can acount for any possible additional maginification within the optical path. You can cange the scaling of the axes from" \
+ " linear to logarithmic according to your prefferences.")
+
+st.write("For ideal sampling performance it is generally understod that the optical resolution limit of the microscope should" \
+ "be 2 - 2.4 times the pixel pitch, which relates to a green to yellow color coding in the graph below. Under this condition, "
+ "or system configuration, respectively, we speek of 'Nyquist Sampling'.")
+
 # Sidebar controls
 st.sidebar.header("Controls")
 
 wavelength = st.sidebar.slider("Wavelength [nm]", 200, 900, 500, step=10)
-pxl_ref = st.sidebar.slider("Pixel size [µm]", 1.0, 13.0, 3.8, step=0.1)
+pxl_ref = st.sidebar.slider("Pixel size [µm]", 1.0, 13.0, 6.5, step=0.1)
 add_mag = st.sidebar.slider("Additional Magnification", 1.0, 5.0, 1.0, step=0.1)
-xscale_choice = st.sidebar.radio("X-axis scale", ["log", "linear"], index=0)
+xscale_choice = st.sidebar.radio("Magnification axis scale", ["log", "linear"], index=0)
+yscale_choice = st.sidebar.radio("NA axis scale", ["log", "linear"], index=1)
+
 
 # Compute and plot
 magnification = list(np.linspace(1, 101, 400))
 num_aprt = list(np.arange(0.095, 1.6, 0.02))
 
-fig = plot_pxl_per_blurr(magnification, num_aprt, wavelength, pxl_ref, add_mag, xscale=xscale_choice)
+fig = plot_pxl_per_blurr(magnification, num_aprt, wavelength, pxl_ref, add_mag, xscale=xscale_choice, yscale=yscale_choice)
 st.pyplot(fig)
 
 
